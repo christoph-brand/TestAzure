138bed63925065e08450111d7a432f27cac3b1f5
christoph-brand <66423764+christoph-brand@users.noreply.github.com>
2024-03-18 16:06:44 +0100

    Update TestAzure.py

--- TestAzure.py
+++ TestAzure.py
@@ -1,2 +1,10 @@
-# python program to print "Hello World" 
-print("Hello World")
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(children='Hello World')
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
